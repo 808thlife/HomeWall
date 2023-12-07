464e51145a8c54b03e59ffddaae4d4e76af11f4f
808thlife <khqyop@gmail.com>
2023-12-07 06:28:17 +0500

    added login view

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     pass
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("auctions:index"))
+        else:
+            return render(request, "accounts/pages-login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "accounts/pages-login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("auctions:index"))
+
