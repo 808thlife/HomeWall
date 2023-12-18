b6c0a6b4e8b8196d7a6d54aad8e0e4e75d84563d
808thlife <khqyop@gmail.com>
2023-12-19 01:23:04 +0500

    backup

--- core/static/core/js/sysinfo.js
+++ core/static/core/js/sysinfo.js
@@ -1,27 +1,16 @@
-// Function to fetch RAM data
-function getRamAndUpdateChart(chartInstance) {
-    fetch("/api/sysinfo/ram")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            updateChartWithRamData(chartInstance, data);
-        })
-        .catch(error => {
-            console.error('Error fetching RAM data:', error);
-        });
+function getRam(){
+    fetch("/api/sysinfo/ram", {
+        method:"GET"
+    })
+    .then(request => request.json())
+    .then(data => {
+        console.log(data)
+    })
+
+
+    //end of the fucntion
 }
 
-// Function to update Chart.js pie chart with RAM data
-function updateChartWithRamData(chartInstance, ramData) {
-    const { availableRAM, usedRAM } = ramData;
 
-    chartInstance.data.datasets[0].data = [usedRAM, availableRAM];
-    chartInstance.update();
-}
+getRam()
 
-// Export the getRamAndUpdateChart function
-window.getRamAndUpdateChart = getRamAndUpdateChart;
