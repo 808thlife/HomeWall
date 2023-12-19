61911ee0485332d38c2d5b176e67c62290d50fb8
808thlife <khqyop@gmail.com>
2023-12-19 20:37:16 +0500

    added line graph

--- core/static/core/js/sysinfo.js
+++ core/static/core/js/sysinfo.js
@@ -53,5 +53,80 @@ document.addEventListener("DOMContentLoaded", async () => {
         });
       }, 2000);
       
+
+
+      // CPU usage
+
+
+
+
+
+
+      //Cpu area graph
+
+
+	
+
+      var ctx = document.getElementById('stackedArea').getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: ['10s', '8s', '6s', '4s', '2s'],
+              datasets: [{
+                  label: 'CPU usage',
+                  data: [0, 20, 40, 10, 15],
+                  backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                  borderColor: 'rgba(75, 192, 192, 1)',
+                  borderWidth: 2,
+                  fill: true,
+              }, ]
+          },
+          options: {
+              scales: {
+                  y: {
+                      title: {
+                          display: true,
+                          text: 'Percentage %'
+                      },
+                      ticks: {
+                          beginAtZero: true,
+                          stepSize: 20,
+                          suggestedMax: 100, // Use suggestedMax instead of max
+                          formatter: function(value) {
+                              return value + '%';
+                          }
+                      }
+                  },
+                  x: {
+                      stacked: true
+                  }
+              },
+              layout: {
+                  padding: {
+                      left: 20,
+                      right: 20,
+                      top: 20,
+                      bottom: 20
+                  }
+              },
+              plugins: {
+                  legend: {
+                      position: 'top',
+                  },
+              }
+          }
+      }); 
+
+    let cpuArr = myChart.data.datasets[0].data
+    console.log(myChart.data.datasets[0].data)
+    cpuArr[0] = 100
+    myChart.data.datasets[0].data[1] = cpuArr
+    console.log(cpuArr[0])
+    cpuArr[1] = 80
+    myChart.update()
+
+
+
+
       //end of the DOM
   });
