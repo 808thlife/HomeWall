9b09d872c40dc775ae94852fc631ee77c39c6d69
808thlife <khqyop@gmail.com>
2024-04-02 19:44:09 +0500

    before breaking everything

--- dhcp/display_ips.py
+++ dhcp/display_ips.py
@@ -1,41 +1,3 @@
-# from scapy.all import ARP, Ether, srp
-# import socket
-
-# # Function to perform ARP scan and gather device information
-# def arp_scan(target_ip):
-#     arp_request = ARP(pdst=target_ip)
-#     ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Broadcast MAC address
-
-#     arp_broadcast = ether/arp_request
-
-#     answered_list = srp(arp_broadcast, timeout=1, verbose=False)[0]
-
-#     devices = []
-#     for element in answered_list:
-#         device_info = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-#         devices.append(device_info)
-
-#     return devices
-
-# # Function to resolve hostname from IP address
-# def resolve_hostname(ip):
-#     try:
-#         hostname, _, _ = socket.gethostbyaddr(ip)
-#         return hostname
-#     except socket.herror:
-#         return None
-
-# # Main function
-# def get_ips():
-#     target_ip = "192.168.1.0/24"  # Example subnet, adjust according to your network
-#     ip_list = []
-#     devices = arp_scan(target_ip)
-
-#     for device in devices:
-#         device["hostname"] = resolve_hostname(device["ip"])
-#         ip_list.append(device)
-#         #print(device)
-#     return ip_list
 
 from scapy.all import ARP, Ether, srp
 import socket
@@ -59,7 +21,8 @@ def scan_network():
     # Resolve hostnames
     for device in devices:
         try:
-            hostname, _, _ = socket.gethostbyaddr(device["ip"])
+            hostname = socket.gethostbyaddr(device["ip"])[0]
+            print(hostname)
             device["hostname"] = hostname
         except socket.herror:
             device["hostname"] = "Unknown"
